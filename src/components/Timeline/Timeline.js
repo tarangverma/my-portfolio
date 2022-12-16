52ede6b63a2fd2b8d44111ebc234760be19dd796
Tarang Verma <tarangverma004@gmail.com>
2022-12-17 02:54:41 +0530

    timeline

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -4,46 +4,96 @@ import "./Timeline.css"
 function Timeline() {
   return (
     <div className='T1'>
-        <h1>PROJECTS</h1>
-        <div className="timeline">
-    <div className="container left">
-      <div className="content-m">
-        <h2>LET'S CHAT </h2>
-        <h3><i>Chat app (1 month)</i></h3>
-        <p>An app to connect with people. It let's us to chat with people just like any other  .</p>
-            <div></div>
+      <h1>PROJECTS</h1>
+      <div className="timeline">
+        <div className="container left">
+          <div className="content-m">
+            <h2>LET'S CHAT </h2>
+            <h3><i>Chat app (1 month)</i></h3>
+            <p>An app to connect with people. It let's us to chat with people just like any other chating apps
+              Also connected it to firebase for backend services.</p>
+            <div className='logos'>
+              <div className='react logo'><img src='https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB'></img></div>
+              <div className='node logo'><img src='https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white'></img></div>
+              <div className='mongodb logo'><img src='https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white'></img></div>
+              <div className='mongoose logo'></div>
+
+            </div>
+            <div className='logos'>
+              <div className='JavaScript logo'><img src='https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E'></img></div>
+              <div className='Html logo'><img src='https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white'></img></div>
+              <div className='Css3 logo'><img src='https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white'></img></div>
+              <div className='firebase logo'></div>
+            </div>
+          </div>
+        </div>
+        <div className="container right">
+          <div className="content-m">
+            <h2>NETFLIX-CLONE</h2>
+            <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum,
+              vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            <div className='logos'>
+              <div className='react logo'><img src='https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB'></img></div>
+              <div className='node logo'><img src='https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white'></img></div>
+              <div className='mongodb logo'><img src='https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white'></img></div>
+              <div className='react-router logo'><img src='https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white'></img></div>
+            </div>
+            <div className='logos'>
+              <div className='express.js logo'><img src='https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB'></img></div>
+
+            </div>
+          </div>
+        </div>
+        <div className="container left">
+          <div className="content-m">
+            <h2>WEATHER FORCAST</h2>
+            <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum,
+              vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            <div className='logos'>
+            <div className='react logo'><img src='https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB'></img></div>
+               <div className='node logo'><img src='https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white'></img></div>
+               <div className='mongodb logo'><img src='https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white'></img></div>
+               <div className='react-router logo'><img src='https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white'></img></div>
+            </div>
+            <div className='logos'>
+
+            </div>
+          </div>
+        </div>
+        <div className="container right">
+          <div className="content-m">
+            <h2>PORTFOLIO</h2>
+            <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum,
+              vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            <div className='logos'>
+            <div className='react logo'><img src='https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB'></img></div>
+               <div className='node logo'><img src='https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white'></img></div>
+               <div className='mongodb logo'><img src='https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white'></img></div>
+               <div className='react-router logo'><img src='https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white'></img></div>
+            </div>
+            <div className='logos'>
+
+            </div>
+          </div>
+        </div>
+        <div className="container left">
+          <div className="content-m">
+            <h2>DISENY+ PAGE</h2>
+            <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum,
+              vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            <div className='logos'>
+            <div className='react logo'><img src='https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB'></img></div>
+               <div className='node logo'><img src='https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white'></img></div>
+               <div className='mongodb logo'><img src='https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white'></img></div>
+               <div className='react-router logo'><img src='https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white'></img></div>
+            </div>
+            <div className='logos'>
+
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <div className="container right">
-      <div className="content-m">
-        <h2>NETFLIX-CLONE</h2>
-        <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, 
-            vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
-      </div>
-    </div>
-    <div className="container left">
-      <div className="content-m">
-        <h2>WEATHER FORCAST</h2>
-        <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, 
-            vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
-      </div>
-    </div>
-    <div className="container right">
-      <div className="content-m">
-        <h2>PORTFOLIO</h2>
-        <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, 
-            vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
-      </div>
-    </div>
-    <div className="container left">
-      <div className="content-m">
-        <h2>DISENY+ PAGE</h2>
-        <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, 
-            vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
-      </div>
-    </div>
-    </div>
-  </div> 
   )
 }
 
